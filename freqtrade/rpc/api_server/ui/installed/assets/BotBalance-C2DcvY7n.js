d5a217e25edf9a9d283d1022078909ae0d3729a1
PiotrPawelw <kontaktsenseagent@gmail.com>
2025-01-25 16:59:37 +0100

    first commit1

---
+++ freqtrade/rpc/api_server/ui/installed/assets/BotBalance-C2DcvY7n.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./BotBalance.vue_vue_type_script_setup_true_lang-DGXGE3OE.js";import"./index-BNCVPFy5.js";import"./installCanvasRenderer-DJntnZUI.js";import"./install-CL3RXg71.js";export{o as default};
+//# sourceMappingURL=BotBalance-C2DcvY7n.js.map
---
+++ freqtrade/rpc/api_server/ui/installed/assets/chevron-down-Bmte9i5Z.js
@@ -0,0 +1,2 @@
+import{V as o,o as n,c as t,a as r}from"./index-BNCVPFy5.js";const a={viewBox:"0 0 24 24",width:"1.2em",height:"1.2em"};function l(s,e){return n(),t("svg",a,e[0]||(e[0]=[r("path",{fill:"currentColor",d:"M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6l-6-6z"},null,-1)]))}const i=o({name:"mdi-chevron-down",render:l});export{i as _};
+//# sourceMappingURL=chevron-down-Bmte9i5Z.js.map
