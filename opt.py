514c9f35dc47f986f8297be8a726319c451e5f0b
PiotrPawelw <kontaktsenseagent@gmail.com>
2025-01-25 12:10:20 +0100

    reformated code

--- opt.py
+++ opt.py
@@ -2,8 +2,10 @@
 import os
 import subprocess
 
+
 # Ścieżka do folderu z strategiami
 strategies_dir = "user_data/strategies/"
+
 
 # Funkcja do uruchamiania hyperoptymalizacji dla każdej strategii
 def optimize_all_strategies():
@@ -18,17 +20,29 @@ def optimize_all_strategies():
 
             # Uruchomienie procesu hiperoptymalizacji za pomocą subprocess
             try:
-                subprocess.run([
-                    "freqtrade", "hyperopt", 
-                    "--strategy", strategy_name,  # Nazwa strategii
-                    "--epochs", "50",
-                    "--spaces", "roi", "stoploss",
-                    "--config", "./1.json",
-		    "--freqaimodel", "ReinforcementLearner_multiproc", 
-		    "--hyperopt-loss", "ShortTradeDurHyperOptLoss",
-                ], check=True)
+                subprocess.run(
+                    [
+                        "freqtrade",
+                        "hyperopt",
+                        "--strategy",
+                        strategy_name,  # Nazwa strategii
+                        "--epochs",
+                        "50",
+                        "--spaces",
+                        "roi",
+                        "stoploss",
+                        "--config",
+                        "./1.json",
+                        "--freqaimodel",
+                        "ReinforcementLearner_multiproc",
+                        "--hyperopt-loss",
+                        "ShortTradeDurHyperOptLoss",
+                    ],
+                    check=True,
+                )
             except subprocess.CalledProcessError as e:
                 print(f"Błąd podczas hiperoptymalizacji dla strategii {strategy_name}: {e}")
+
 
 if __name__ == "__main__":
     optimize_all_strategies()
